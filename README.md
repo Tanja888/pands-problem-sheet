05ca52c5bb6c5c1fb4496b5a7eddbab6e39fec3b
Tanja <G00411329@gmit.ie>
2022-02-16 23:56:42 +0000

    Weekly Task 04

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # pands-problem-sheet
-    **bmi.py**
-    
+    ##bmi.py##
+
     Solution to the Weekly Task 02
         Calculates the BMI (Body Mass Index)
 
-
+___
     **secondstring.py**
 
     Solution to the Weekly Task 03
         Inputs string and outputs every second letter in reverse order
+
+
+    ##collatz.py##
+
+    Solution to the Weekly Task 04
+        Inputs positive integers and outputs results based on the Collatz Conjecture
---
+++ collatz.py
@@ -0,0 +1,19 @@
+# Collatz 
+# Author: Tanja Juric
+
+number = int(input("Please enter a positive integer: "))
+
+# Variable number has to be different than 1 to enter the while loop
+# If number is 1 the program will end
+while number != 1:   
+
+# if the number is even divide it by 2
+# introduced the variable result because that will represent the changed variable 'number' which is being checked in the condition of the while
+    if number % 2 == 0: 
+        result = int(number / 2)
+    else: 
+        result = (number * 3) + 1
+
+    print(result)
+    number = result
+print ("The End") # when the result reached False (= 1) print 'The End' because the loop ended
