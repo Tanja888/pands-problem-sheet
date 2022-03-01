cad1dfa5a7d3bdef88a49b6fbb8da0c87fd77560
Tanja <G00411329@gmit.ie>
2022-03-01 14:13:58 +0000

    Weekly Task 05

--- weekday.py
+++ weekday.py
@@ -1,7 +1,7 @@
 # Weekly task 05 - Wether or not today is a weekday
 # Author: Tanja Juric
 
-# I don't get yet what I did - took it from here: 
+# I took the function from here: 
 # URL: https://stackoverflow.com/questions/8380389/how-to-get-day-name-from-datetime
 # The result is good - it shows the correct day
 # %A shows full week day name, URL: https://www.programiz.com/python-programming/datetime/strptime
@@ -9,6 +9,12 @@
 import datetime
 
 now = datetime.datetime.now() 
-print (now.strftime("%A")) #strftime() converts date and time objects to their string representation
+day = now.strftime("%A")
+print (day)
+#strftime() converts date and time objects to their string representation
 
-# now I need to separate working days from weekend and include 'now' somehow
+if day == "Saturday" or day == "Sunday":
+    print ("Yay, today is weekend!")
+
+else:
+    print ("Unfortunately, today is a working day.")
