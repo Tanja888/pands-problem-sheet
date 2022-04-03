86bd4461861c30cd4f879f10df0570d5fb727571
Tanja <G00411329@gmit.ie>
2022-04-03 14:56:15 +0100

    Changing README file

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # pands-problem-sheet
-##  bmi.py
+##  bmi.py 
 
-    Solution to the Weekly Task 02
-        Calculates the BMI (Body Mass Index)
+###    Solution to the Weekly Task 02
+        Calculates the BMI (Body Mass Index) :weight_lifting:
+
+###    References
+For this task I used Stackoverflow to get the [BMI calculator][1].
+I checked Codegrepper to [round the result][2].  
+
+[1]: https://stackoverflow.com/questions/20405610/bmi-calculator-in-python/20405792
+[2]: https://www.codegrepper.com/code-examples/python/how+to+round+with+format+in+python
 
 ___
 ##  secondstring.py 
--- bmi.py
+++ bmi.py
@@ -4,8 +4,8 @@
 
 print ('Hello! Today we are going to calculate your Body Mass Index, known as BMI.')
 
-height = int(input('Please enter your height in centimetres: ')) #not sure if float or int
-weight = float(input('Please enter your weight in kilograms: ')) #not sure if float or int
+height = float(input('Please enter your height in centimetres: ')) 
+weight = float(input('Please enter your weight in kilograms: ')) 
 
 heightInMeters = height/100
 powerOfMeters = heightInMeters ** 2
@@ -22,9 +22,6 @@ print ('Your BMI is: {}' .format(bmi))
 print ('Your BMI is ' + str(bmi))
 
 
-# not sure where to put references
-# URL: https://stackoverflow.com/questions/20405610/bmi-calculator-in-python/20405792
-# URL: https://www.codegrepper.com/code-examples/python/how+to+round+with+format+in+python
 
 
 
